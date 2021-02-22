ec08ca73f2284f57c4f919da0760b30686bd2bc1
ngotiencong <75078802+ngotiencong@users.noreply.github.com>
2021-02-22 17:15:20 +0700

    Update 01.js

--- 01.js
+++ 01.js
@@ -1,1 +1,46 @@
+$(document).ready(function() {
+    $('.courses__box').owlCarousel({
+        animateOut: 'slideOutDown',
+        animateIn: 'flipInX',
+        items: 1,
+        loop: true,
+        margin: 10,
+        responsiveClass: true,
+        nav: false,
+        dotsEachdotsEach: 2,
 
+        stagePadding: 30,
+        smartSpeed: 450,
+        responsive: {
+            0: {
+                items: 1,
+
+            },
+            601: {
+                items: 2,
+
+            },
+            900: {
+                items: 3,
+
+                loop: true
+            },
+            1200: {
+                items: 4
+            }
+        }
+    });
+    $('.rate__content').owlCarousel({
+        animateOut: 'slideOutDown',
+        animateIn: 'flipInX',
+        items: 1,
+        margin: 30,
+        loop: true,
+        stagePadding: 30,
+        smartSpeed: 450,
+        nav: false,
+        dots: false
+
+
+    });
+});
